17e704715a4c212532c271c98cb64207ddc0c56c
jerry8282 <ka06196@naver.com>
2025-12-07 20:23:26 +0900

    chore: JVM Target 17으로 상향 조정

--- Dialog/composeApp/build.gradle.kts
+++ Dialog/composeApp/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 kotlin {
     androidTarget {
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
     
@@ -68,8 +68,8 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
