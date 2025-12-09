1763c343f6e2a93c46d58d6e52916c8fd23cf295
jerry8282 <ka06196@naver.com>
2025-12-10 02:54:32 +0900

    refactor: 코드 스타일 정리

--- Dialog/composeApp/src/commonMain/kotlin/com/on/dialog/App.kt
+++ Dialog/composeApp/src/commonMain/kotlin/com/on/dialog/App.kt
@@ -13,11 +13,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.ui.tooling.preview.Preview
-
 import dialog.composeapp.generated.resources.Res
 import dialog.composeapp.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
@@ -25,10 +24,11 @@ fun App() {
     MaterialTheme {
         var showContent by remember { mutableStateOf(false) }
         Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
+            modifier =
+                Modifier
+                    .background(MaterialTheme.colorScheme.primaryContainer)
+                    .safeContentPadding()
+                    .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Button(onClick = { showContent = !showContent }) {
