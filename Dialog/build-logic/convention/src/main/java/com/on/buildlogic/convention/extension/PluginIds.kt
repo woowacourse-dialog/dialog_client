aa87b7ecedc874942739c71b314acf3b6ef98f6c
moondev03 <moondev03@gmail.com>
2025-12-15 16:45:17 +0900

    feat: PluginIds object 추가
    * PluginIds.kt: 빌드 로직에서 사용될 플러그인 ID 상수들을 관리하는 PluginIds object 생성

---
+++ Dialog/build-logic/convention/src/main/java/com/on/buildlogic/convention/extension/PluginIds.kt
@@ -0,0 +1,10 @@
+package com.on.buildlogic.convention.extension
+
+internal object PluginIds {
+    const val KOTLIN_MULTIPLATFORM = "org.jetbrains.kotlin.multiplatform"
+    const val ANDROID_APPLICATION = "com.android.application"
+    const val ANDROID_LIBRARY = "com.android.library"
+
+    const val COMPOSE_MULTIPLATFORM = "org.jetbrains.compose"
+    const val KOTLIN_COMPOSE = "org.jetbrains.kotlin.plugin.compose"
+}
