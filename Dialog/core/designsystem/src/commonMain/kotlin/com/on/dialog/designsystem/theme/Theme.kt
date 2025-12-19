ead3e7c7e6a9df0f2dd8e6fc747dfe63a3b8d1b4
ijh1298 <imjh0417@gmail.com>
2025-12-19 22:05:44 +0900

    refactor: 디자인 시스템 테마의 동적 색상(Dynamic Color) 비활성화
    * `core/designsystem`: `DialogTheme`의 `dynamicColor` 기본값을 `false`로 변경하여 비활성화

--- Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/theme/Theme.kt
+++ Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/theme/Theme.kt
@@ -92,7 +92,7 @@ expect fun getDynamicColorScheme(darkTheme: Boolean): ColorScheme?
 fun DialogTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit,
 ) {
     val colorScheme =
