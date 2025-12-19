d8884d17ef22f669e356fa7a528a8aa2c607a1c1
ijh1298 <imjh0417@gmail.com>
2025-12-19 22:07:07 +0900

    feat: Icons 및 버튼 Component 추가

--- Dialog/composeApp/src/commonMain/kotlin/com/on/dialog/App.kt
+++ Dialog/composeApp/src/commonMain/kotlin/com/on/dialog/App.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,6 +16,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import com.on.dialog.designsystem.component.DialogButton
 import com.on.dialog.designsystem.theme.DialogTheme
 import dialog.composeapp.generated.resources.Res
 import dialog.composeapp.generated.resources.compose_multiplatform
@@ -31,12 +31,12 @@ fun App() {
         Column(
             modifier =
                 Modifier
-                    .background(MaterialTheme.colorScheme.primaryContainer)
+                    .background(MaterialTheme.colorScheme.onPrimary)
                     .safeContentPadding()
                     .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Button(onClick = { showContent = !showContent }) {
+            DialogButton(onClick = { showContent = !showContent }) {
                 Text("Click me!")
             }
             AnimatedVisibility(showContent) {
---
+++ Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/component/Button.kt
@@ -0,0 +1,193 @@
+package com.on.dialog.designsystem.component
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
+import com.on.dialog.designsystem.icon.DialogIcons
+import com.on.dialog.designsystem.theme.DialogTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DialogButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    shape: Shape = RoundedCornerShape(DialogButtonDefaults.CornerRadius),
+    content: @Composable RowScope.() -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors =
+            ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+            ),
+        contentPadding = contentPadding,
+        content = content,
+        shape = shape,
+    )
+}
+
+@Composable
+fun DialogButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    text: @Composable () -> Unit,
+    leadingIcon: @Composable (() -> Unit)? = null,
+) {
+    DialogButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        contentPadding =
+            if (leadingIcon != null) {
+                ButtonDefaults.ButtonWithIconContentPadding
+            } else {
+                ButtonDefaults.ContentPadding
+            },
+    ) {
+        DialogButtonContext(
+            text = text,
+            leadingIcon = leadingIcon,
+        )
+    }
+}
+
+@Composable
+fun DialogOutlinedButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    shape: Shape = RoundedCornerShape(DialogButtonDefaults.CornerRadius),
+    content: @Composable RowScope.() -> Unit,
+) {
+    OutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        colors =
+            ButtonDefaults.outlinedButtonColors(
+                contentColor = MaterialTheme.colorScheme.onBackground,
+            ),
+        border =
+            BorderStroke(
+                width = DialogButtonDefaults.OutlinedButtonBorderWidth,
+                color =
+                    if (enabled) {
+                        MaterialTheme.colorScheme.outline
+                    } else {
+                        MaterialTheme.colorScheme.onSurface.copy(
+                            alpha = DialogButtonDefaults.DISABLED_OUTLINED_BUTTON_BORDER_ALPHA,
+                        )
+                    },
+            ),
+        contentPadding = contentPadding,
+        shape = shape,
+        content = content,
+    )
+}
+
+@Composable
+fun DialogOutlinedButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    text: @Composable () -> Unit,
+    leadingIcon: @Composable (() -> Unit)? = null,
+) {
+    DialogOutlinedButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        contentPadding =
+            if (leadingIcon != null) {
+                ButtonDefaults.ButtonWithIconContentPadding
+            } else {
+                ButtonDefaults.ContentPadding
+            },
+    ) {
+        DialogButtonContext(
+            text = text,
+            leadingIcon = leadingIcon,
+        )
+    }
+}
+
+@Composable
+private fun DialogButtonContext(
+    text: @Composable () -> Unit,
+    leadingIcon: @Composable (() -> Unit)? = null,
+) {
+    if (leadingIcon != null) {
+        Box(Modifier.sizeIn(maxHeight = ButtonDefaults.IconSize)) {
+            leadingIcon()
+        }
+    }
+    Box(
+        Modifier
+            .padding(
+                start =
+                    if (leadingIcon != null) {
+                        ButtonDefaults.IconSpacing
+                    } else {
+                        0.dp
+                    },
+            ),
+    ) {
+        text()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DialogButtonPreview() {
+    DialogTheme {
+        DialogButton(onClick = {}, text = { Text("다이얼로그") })
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DialogOutlinedButtonPreview() {
+    DialogTheme {
+        DialogOutlinedButton(onClick = {}, text = { Text("로그인") })
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DialogButtonLeadingIconPreview() {
+    DialogTheme {
+        DialogButton(
+            onClick = {},
+            text = { Text("추가") },
+            leadingIcon = {
+                Icon(imageVector = DialogIcons.Add, contentDescription = null)
+            },
+        )
+    }
+}
+
+object DialogButtonDefaults {
+    const val DISABLED_OUTLINED_BUTTON_BORDER_ALPHA = 0.12f
+    val CornerRadius = 8.dp
+    val OutlinedButtonBorderWidth = 1.dp
+}
