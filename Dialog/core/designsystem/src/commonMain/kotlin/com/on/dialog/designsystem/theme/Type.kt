77058db14556ed10077817c19736cdb84810f893
ijh1298 <imjh0417@gmail.com>
2025-12-26 14:16:24 +0900

    refactor: sp 단위를 dp로 변경
    * `core/designsystem/theme/Type.kt`: Typography의 모든 `sp` 단위를 `dp`로 변환하여 적용

--- Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/theme/Type.kt
+++ Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/theme/Type.kt
@@ -2,12 +2,18 @@ package com.on.dialog.designsystem.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.on.dialog.designsystem.theme.preview.TypographyDarkPreview
 import com.on.dialog.designsystem.theme.preview.TypographyPreview
 import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+private fun dpToSp(dp: Dp) = with(LocalDensity.current) { dp.toSp() }
 
 internal val DialogTypography
     @Composable
@@ -17,125 +23,125 @@ internal val DialogTypography
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Normal,
-                    fontSize = 57.sp,
-                    lineHeight = 64.sp,
-                    letterSpacing = (-0.25).sp,
+                    fontSize = dpToSp(57.dp),
+                    lineHeight = dpToSp(64.dp),
+                    letterSpacing = dpToSp((-0.25).dp),
                 ),
             displayMedium =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Normal,
-                    fontSize = 45.sp,
-                    lineHeight = 52.sp,
-                    letterSpacing = 0.sp,
+                    fontSize = dpToSp(45.dp),
+                    lineHeight = dpToSp(52.dp),
+                    letterSpacing = dpToSp(0.dp),
                 ),
             displaySmall =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Normal,
-                    fontSize = 36.sp,
-                    lineHeight = 44.sp,
-                    letterSpacing = 0.sp,
+                    fontSize = dpToSp(36.dp),
+                    lineHeight = dpToSp(44.dp),
+                    letterSpacing = dpToSp(0.dp),
                 ),
             headlineLarge =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.SemiBold,
-                    fontSize = 32.sp,
-                    lineHeight = 40.sp,
-                    letterSpacing = 0.sp,
+                    fontSize = dpToSp(32.dp),
+                    lineHeight = dpToSp(40.dp),
+                    letterSpacing = dpToSp(0.dp),
                 ),
             headlineMedium =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.SemiBold,
-                    fontSize = 28.sp,
-                    lineHeight = 36.sp,
-                    letterSpacing = 0.sp,
+                    fontSize = dpToSp(28.dp),
+                    lineHeight = dpToSp(36.dp),
+                    letterSpacing = dpToSp(0.dp),
                 ),
             headlineSmall =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.SemiBold,
-                    fontSize = 24.sp,
-                    lineHeight = 32.sp,
-                    letterSpacing = 0.sp,
+                    fontSize = dpToSp(24.dp),
+                    lineHeight = dpToSp(32.dp),
+                    letterSpacing = dpToSp(0.dp),
                 ),
             titleLarge =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.SemiBold,
-                    fontSize = 22.sp,
-                    lineHeight = 28.sp,
-                    letterSpacing = 0.sp,
+                    fontSize = dpToSp(22.dp),
+                    lineHeight = dpToSp(28.dp),
+                    letterSpacing = dpToSp(0.dp),
                 ),
             titleMedium =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Medium,
-                    fontSize = 16.sp,
-                    lineHeight = 24.sp,
-                    letterSpacing = 0.15.sp,
+                    fontSize = dpToSp(16.dp),
+                    lineHeight = dpToSp(24.dp),
+                    letterSpacing = dpToSp(0.15.dp),
                 ),
             titleSmall =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Medium,
-                    fontSize = 14.sp,
-                    lineHeight = 20.sp,
-                    letterSpacing = 0.1.sp,
+                    fontSize = dpToSp(14.dp),
+                    lineHeight = dpToSp(20.dp),
+                    letterSpacing = dpToSp(0.1.dp),
                 ),
             // Default Text Style
             bodyLarge =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Normal,
-                    fontSize = 16.sp,
-                    lineHeight = 24.sp,
-                    letterSpacing = 0.5.sp,
+                    fontSize = dpToSp(16.dp),
+                    lineHeight = dpToSp(24.dp),
+                    letterSpacing = dpToSp(0.5.dp),
                 ),
             bodyMedium =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Normal,
-                    fontSize = 14.sp,
-                    lineHeight = 20.sp,
-                    letterSpacing = 0.25.sp,
+                    fontSize = dpToSp(14.dp),
+                    lineHeight = dpToSp(20.dp),
+                    letterSpacing = dpToSp(0.25.dp),
                 ),
             bodySmall =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Normal,
-                    fontSize = 12.sp,
-                    lineHeight = 16.sp,
-                    letterSpacing = 0.4.sp,
+                    fontSize = dpToSp(12.dp),
+                    lineHeight = dpToSp(16.dp),
+                    letterSpacing = dpToSp(0.4.dp),
                 ),
             // Used for Button
             labelLarge =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Medium,
-                    fontSize = 14.sp,
-                    lineHeight = 20.sp,
-                    letterSpacing = 0.1.sp,
+                    fontSize = dpToSp(14.dp),
+                    lineHeight = dpToSp(20.dp),
+                    letterSpacing = dpToSp(0.1.dp),
                 ),
             // Used for Navigation Items
             labelMedium =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Medium,
-                    fontSize = 12.sp,
-                    lineHeight = 16.sp,
-                    letterSpacing = 0.5.sp,
+                    fontSize = dpToSp(12.dp),
+                    lineHeight = dpToSp(16.dp),
+                    letterSpacing = dpToSp(0.5.dp),
                 ),
             // Used for Tag
             labelSmall =
                 TextStyle(
                     fontFamily = pretendardFontFamily(),
                     fontWeight = FontWeight.Medium,
-                    fontSize = 11.sp,
-                    lineHeight = 16.sp,
-                    letterSpacing = 0.5.sp,
+                    fontSize = dpToSp(11.dp),
+                    lineHeight = dpToSp(16.dp),
+                    letterSpacing = dpToSp(0.5.dp),
                 ),
         )
 
