eaca8895ad58e97f0a8188d47c9fdabc37f8f960
jerry8282 <ka06196@naver.com>
2026-01-03 16:14:18 +0900

    feat: core-network 모듈 추가 및 HttpClient 설정

---
+++ Dialog/core/network/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("dialog.convention.kmp.library")
+}
+
+android {
+    namespace = "com.on.dialog.core.network"
+}
+
+kotlin {
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.ktor.client.core)
+            implementation(libs.ktor.client.content.negotiation)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.ktor.serialization.kotlinx.json)
+            implementation(project(":core:data"))
+        }
+        androidMain.dependencies {
+            implementation(libs.ktor.client.okhttp)
+        }
+        iosMain.dependencies {
+            implementation(libs.ktor.client.darwin)
+        }
+    }
+}
