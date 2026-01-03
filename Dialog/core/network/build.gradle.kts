349e68b5d371b07f053b1a3b94016ad1680184f5
jerry8282 <ka06196@naver.com>
2026-01-03 16:31:08 +0900

    chore: Network 모듈에 Ktorfit 라이브러리 추가

--- Dialog/core/network/build.gradle.kts
+++ Dialog/core/network/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("dialog.convention.kmp.library")
+    alias(libs.plugins.ksp.gradle.plugin)
 }
 
 android {
@@ -9,10 +10,16 @@ android {
 kotlin {
     sourceSets {
         commonMain.dependencies {
+            // ktor
             implementation(libs.ktor.client.core)
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.kotlinx.serialization.json)
             implementation(libs.ktor.serialization.kotlinx.json)
+
+            // ktorfit
+            implementation(libs.ktorfit)
+
+            // project
             implementation(project(":core:data"))
         }
         androidMain.dependencies {
@@ -22,4 +29,14 @@ kotlin {
             implementation(libs.ktor.client.darwin)
         }
     }
+}
+
+dependencies {
+    //Android
+    kspAndroid(libs.ktorfit.ksp)
+
+    //Ios
+    kspIosSimulatorArm64(libs.ktorfit.ksp)
+    kspIosArm64(libs.ktorfit.ksp)
+    kspIosSimulatorArm64(libs.ktorfit.ksp)
 }
