120ce2b51d81dcf92f5dc73121975faa16a29b77
jerry8282 <ka06196@naver.com>
2026-01-07 05:02:47 +0900

    refactor: Network 로깅 개선 (Pretty Print 적용)

--- Dialog/core/network/src/androidMain/kotlin/com/on/network/di/HttpClientFactory.kt
+++ Dialog/core/network/src/androidMain/kotlin/com/on/network/di/HttpClientFactory.kt
@@ -1,13 +1,8 @@
 package com.on.network.di
 
-import com.on.dialog.core.network.BuildKonfig
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.DEFAULT
-import io.ktor.client.plugins.logging.LogLevel
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.client.plugins.logging.Logging
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -23,12 +18,6 @@ actual object HttpClientFactory {
                     },
                 )
             }
-
-            if (BuildKonfig.IS_DEBUG) {
-                install(Logging) {
-                    logger = Logger.DEFAULT
-                    level = LogLevel.ALL
-                }
-            }
+            installLogging()
         }
 }
--- Dialog/core/network/src/commonMain/kotlin/com/on/network/di/HttpClientFactory.kt
+++ Dialog/core/network/src/commonMain/kotlin/com/on/network/di/HttpClientFactory.kt
@@ -1,7 +1,76 @@
 package com.on.network.di
 
+import com.on.dialog.core.network.BuildKonfig
+import io.github.aakira.napier.Napier
 import io.ktor.client.HttpClient
+import io.ktor.client.HttpClientConfig
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
 
 expect object HttpClientFactory {
     fun create(): HttpClient
+}
+
+internal fun HttpClientConfig<*>.installLogging() {
+    if (!BuildKonfig.IS_DEBUG) return
+    install(Logging) {
+        logger = PrettyLogger
+        level = LogLevel.ALL
+    }
+}
+
+private fun Json.prettyJson(json: String): String = try {
+    val parsed = parseToJsonElement(json)
+    encodeToString(JsonElement.serializer(), parsed)
+} catch (e: Exception) {
+    json
+}
+
+private object PrettyLogger : Logger {
+    private val jsonConfiguration = Json {
+        prettyPrintIndent = "\t"
+        prettyPrint = true
+    }
+
+    override fun log(message: String) {
+        val replacedMessage = replaceBodyWithPrettyJson(message)
+        Napier.v(tag = "KtorLogger", message = replacedMessage)
+    }
+
+    private fun replaceBodyWithPrettyJson(message: String): String {
+        val startToken = "BODY START"
+        val endToken = "BODY END"
+
+        val startIndex: Int = message.indexOf(startToken)
+        val endIndex: Int = message.indexOf(endToken)
+
+        if (startIndex == -1 || endIndex == -1 || startIndex >= endIndex) {
+            return message
+        }
+
+        val bodyStart: Int = startIndex + startToken.length
+        val rawBody: String = message.substring(bodyStart, endIndex).trim()
+
+        val prettyBody: String = runCatching {
+            jsonConfiguration.prettyJson(rawBody)
+        }.getOrElse {
+            rawBody
+        }
+
+        val before: String = message.substring(0, bodyStart)
+        val after: String = message.substring(endIndex)
+
+        return buildString {
+            appendLine("----------".repeat(10))
+            append(before)
+            append("\n")
+            append(prettyBody.prependIndent("\t"))
+            append("\n")
+            appendLine(after)
+            appendLine("----------".repeat(10))
+        }
+    }
 }
