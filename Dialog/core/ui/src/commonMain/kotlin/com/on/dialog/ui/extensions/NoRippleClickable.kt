885235fa6401aab7f7950184f9520c3ce61b6192
MunJangHun <105299421+moondev03@users.noreply.github.com>
2026-01-07 21:42:25 +0900

    Fix ktlint import order

--- Dialog/core/ui/src/commonMain/kotlin/com/on/dialog/ui/extensions/NoRippleClickable.kt
+++ Dialog/core/ui/src/commonMain/kotlin/com/on/dialog/ui/extensions/NoRippleClickable.kt
@@ -1,7 +1,7 @@
 package com.on.dialog.ui.extensions
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
