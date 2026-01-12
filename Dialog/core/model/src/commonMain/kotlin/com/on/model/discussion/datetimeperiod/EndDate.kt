07b84233bb6a75ad8fd2359058ebc6cf9484d875
jerry8282 <ka06196@naver.com>
2026-01-12 19:55:13 +0900

    feat: EndDate 모델 추가

---
+++ Dialog/core/model/src/commonMain/kotlin/com/on/model/discussion/datetimeperiod/EndDate.kt
@@ -0,0 +1,17 @@
+package com.on.model.discussion.datetimeperiod
+
+import com.on.dialog.core.common.extension.atEndOfDay
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.LocalDateTime
+import kotlin.jvm.JvmInline
+
+@JvmInline
+value class EndDate(
+    val endDate: LocalDate
+) {
+    fun isInPeriod(startAt: LocalDateTime, dateTime: LocalDateTime): Boolean =
+        dateTime in startAt..endDate.atEndOfDay()
+
+    fun isAfterEnd(dateTime: LocalDateTime): Boolean =
+        dateTime > endDate.atEndOfDay()
+}
