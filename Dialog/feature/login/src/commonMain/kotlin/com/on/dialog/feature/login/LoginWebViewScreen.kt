83e0c8e9bd58b1aa881910485fcb28d79f0f8648
ijh1298 <imjh0417@gmail.com>
2026-01-18 13:56:44 +0900

    feat: LoginWebViewScreen 콜백 매개변수 추가 및 KDoc 업데이트

--- Dialog/feature/login/src/commonMain/kotlin/com/on/dialog/feature/login/LoginWebViewScreen.kt
+++ Dialog/feature/login/src/commonMain/kotlin/com/on/dialog/feature/login/LoginWebViewScreen.kt
@@ -14,8 +14,11 @@ enum class LoginType(
  * 지정된 [LoginType]으로 소셜 로그인을 수행하는 웹뷰 화면입니다.
  *
  * @param loginType 로그인 방식 (GitHub, Google 등)
- * @param onLoginSuccess 로그인 성공 시 호출되며, 세션 ID(JSESSIONID)를 전달합니다.
- * @param onLoginFailure 로그인 실패 또는 취소 시 호출됩니다.
+ * @param onLoginSuccess 로그인 성공 시 호출되는 콜백
+ * @param onLoginFailure 로그인 실패 시 호출되는 콜백
+ * @param onLoginCancel 로그인 취소 시 호출되는 콜백
+ * @param onSignUp 회원가입이 필요한 경우 호출되는 콜백
+ * @param viewModel 로그인 로직을 관리하는 뷰모델
  */
 @Composable
 expect fun LoginWebViewScreen(
