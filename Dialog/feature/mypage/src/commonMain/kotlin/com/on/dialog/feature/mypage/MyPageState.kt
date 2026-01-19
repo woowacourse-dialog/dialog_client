f0d563ea7e3d095c8a41b58c2202e29fd352d39c
ijh1298 <imjh0417@gmail.com>
2026-01-20 03:06:38 +0900

    refactor: LoginState 및 MyPageState에 @Immutable 어노테이션 추가 및 필드 확장
    - `LoginState`에 `@Immutable` 추가 및 `isLoginComplete`, `isNewUser` 필드 정의
    - `MyPageState`에 `@Immutable` 어노테이션 추가

--- Dialog/feature/mypage/src/commonMain/kotlin/com/on/dialog/feature/mypage/MyPageState.kt
+++ Dialog/feature/mypage/src/commonMain/kotlin/com/on/dialog/feature/mypage/MyPageState.kt
@@ -1,7 +1,9 @@
 package com.on.dialog.feature.mypage
 
+import androidx.compose.runtime.Immutable
 import com.on.dialog.ui.viewmodel.UiState
 
+@Immutable
 data class MyPageState(
     val isLoggedIn: Boolean = false,
     val isLoading: Boolean = true,
