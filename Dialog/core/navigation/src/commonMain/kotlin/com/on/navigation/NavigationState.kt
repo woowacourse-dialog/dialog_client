ebc413fb6afdaf75ca95a6be20a50508106b0a68
jerry8282 <ka06196@naver.com>
2026-01-23 21:56:16 +0900

    docs: NavigationState 클래스 내 속성 설명을 위한 KDoc 주석 추가

--- Dialog/core/navigation/src/commonMain/kotlin/com/on/navigation/NavigationState.kt
+++ Dialog/core/navigation/src/commonMain/kotlin/com/on/navigation/NavigationState.kt
@@ -42,6 +42,10 @@ fun rememberNavigationState(
  * @param startKey - 앱의 시작 라우트. 사용자는 이 라우트를 통해 앱을 종료하게 된다.
  * @param topLevelStack - 현재 선택된 최상위(Top-level) 라우트
  * @param subStacks - 각 최상위 라우트에 대응되는 백 스택들
+ * @param currentTopLevelKey - 현재 선택된 최상위 라우트
+ * @param currentKey - 현재 선택된 라우트
+ * @param topLevelKeys - 최상위 라우트들
+ * @param currentSubStack - 현재 선택된 최상위 라우트의 백 스택
  */
 
 class NavigationState(
