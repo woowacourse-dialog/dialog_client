2adaf759c350f978766e9e65b3e9f7b653c76905
moondev03 <moondev03@gmail.com>
2026-01-30 14:13:10 +0900

    feat: iOS 진입점 `App`에서 `MainApp`으로 변경
    - composeApp/src/iosMain/kotlin/com/on/dialog/MainViewController.kt: iOS 앱의 시작점을 기존 `App` 컴포저블에서 `MainApp` 컴포저블로 변경

--- Dialog/composeApp/src/iosMain/kotlin/com/on/dialog/MainViewController.kt
+++ Dialog/composeApp/src/iosMain/kotlin/com/on/dialog/MainViewController.kt
@@ -3,10 +3,11 @@ package com.on.dialog
 import androidx.compose.ui.window.ComposeUIViewController
 import com.on.dialog.di.initKoin
 import com.on.dialog.di.initLogger
+import com.on.dialog.main.MainApp
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
         initLogger()
         initKoin()
     }
-) { App() }
+) { MainApp() }
