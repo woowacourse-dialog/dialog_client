b82a57f12bc75a456091863551e8aaefedd0fc05
moondev03 <moondev03@gmail.com>
2026-02-03 14:35:42 +0900

    refactor: DiscussionTypeUiModel이 문자열 리소스를 사용하도록 변경
    - `feature/discussionlist`:
        - `DiscussionTypeUiModel` enum의 `title` 프로퍼티를 하드코딩된 문자열에서 `StringResource`를 사용하도록 변경했습니다.
        - `strings.xml`에 `discussion_type_online` 및 `discussion_type_offline` 문자열 리소스를 추가했습니다.
        - 사용하지 않는 `DiscussionType.toUiModel()` 확장 함수를 제거했습니다.

--- Dialog/feature/discussionlist/impl/src/commonMain/kotlin/com/on/dialog/feature/discussionlist/impl/model/DiscussionTypeUiModel.kt
+++ Dialog/feature/discussionlist/impl/src/commonMain/kotlin/com/on/dialog/feature/discussionlist/impl/model/DiscussionTypeUiModel.kt
@@ -1,23 +1,25 @@
 package com.on.dialog.feature.discussionlist.impl.model
 
+import androidx.compose.runtime.Composable
 import com.on.dialog.model.discussion.content.DiscussionType
+import dialog.feature.discussionlist.impl.generated.resources.Res
+import dialog.feature.discussionlist.impl.generated.resources.discussion_type_offline
+import dialog.feature.discussionlist.impl.generated.resources.discussion_type_online
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.stringResource
 
 internal enum class DiscussionTypeUiModel(
-    val title: String,
+    val titleResId: StringResource,
 ) {
-    ONLINE(title = "온라인"),
-    OFFLINE(title = "오프라인"),
+    ONLINE(titleResId = Res.string.discussion_type_online),
+    OFFLINE(titleResId = Res.string.discussion_type_offline),
     ;
+
+    val title: String
+        @Composable get() = stringResource(titleResId)
 
     fun toDomain(): DiscussionType = when (this) {
         ONLINE -> DiscussionType.ONLINE
         OFFLINE -> DiscussionType.OFFLINE
-    }
-
-    companion object {
-        fun DiscussionType.toUiModel(): DiscussionTypeUiModel = when (this) {
-            DiscussionType.ONLINE -> ONLINE
-            DiscussionType.OFFLINE -> OFFLINE
-        }
     }
 }
