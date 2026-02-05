e8da9289dff54fc8e7628f42a83a46f85fe520e2
jerry8282 <ka06196@naver.com>
2026-02-06 06:13:59 +0900

    feat: AnimatedTab 컴포넌트 Preview 추가

--- Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/component/AnimatedTab.kt
+++ Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/component/AnimatedTab.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SecondaryTabRow
 import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import com.on.dialog.designsystem.theme.DialogTheme
 
 @Composable
@@ -48,4 +50,40 @@ fun AnimatedTab(
             )
         },
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DialogButtonPreviewLight() {
+    DialogTheme {
+        AnimatedTabPreviewContent()
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0)
+@Composable
+private fun DialogButtonPreviewDark() {
+    DialogTheme(darkTheme = true) {
+        AnimatedTabPreviewContent()
+    }
+}
+
+@Composable
+private fun AnimatedTabPreviewContent() {
+    SecondaryTabRow(
+        selectedTabIndex = 0,
+        indicator = {},
+        divider = {}
+    ) {
+        AnimatedTab(
+            title = "작성",
+            selected = true,
+            onClick = {}
+        )
+        AnimatedTab(
+            title = "미리보기",
+            selected = false,
+            onClick = {}
+        )
+    }
 }
