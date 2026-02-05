eddc11012e88598f91b4ad28766ff6bd0437fc62
jerry8282 <ka06196@naver.com>
2026-02-06 05:02:22 +0900

    refactor: MarkdownButton 내 Icon 크기 지정 방식을 fillMaxSize로 변경

--- Dialog/core/ui/src/commonMain/kotlin/com/on/dialog/ui/component/button/MarkdownButton.kt
+++ Dialog/core/ui/src/commonMain/kotlin/com/on/dialog/ui/component/button/MarkdownButton.kt
@@ -1,12 +1,11 @@
 package com.on.dialog.ui.component.button
 
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.unit.dp
 import com.on.dialog.designsystem.component.DialogIconButton
 import com.on.dialog.designsystem.component.DialogIconButtonTone
 
@@ -26,8 +25,7 @@ fun MarkdownButton(
         },
         content = {
             Icon(
-                // TODO 버튼 크게 맞게 사이즈 넣기
-                modifier = Modifier.size(30.dp),
+                modifier = Modifier.fillMaxSize(0.6f),
                 imageVector = icon,
                 contentDescription = null
             )
