fc901dcb4632abfa5e046050bde51757bbb329f6
jerry8282 <ka06196@naver.com>
2026-02-06 03:53:20 +0900

    feat: 마크다운 미리보기 기능이 포함된 `MarkdownPreviewView` 컴포저블 추가

---
+++ Dialog/core/ui/src/commonMain/kotlin/com/on/dialog/ui/component/MarkdownPreviewView.kt
@@ -0,0 +1,105 @@
+package com.on.dialog.ui.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SecondaryTabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.mikepenz.markdown.compose.Markdown
+import com.mikepenz.markdown.m3.markdownColor
+import com.mikepenz.markdown.m3.markdownTypography
+import com.on.dialog.designsystem.component.AnimatedTab
+import com.on.dialog.designsystem.theme.DialogTheme
+
+@Composable
+fun MarkdownPreviewView(
+    text: String,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        var selectedTabIndex: Int by remember { mutableStateOf(0) }
+
+        SecondaryTabRow(
+            selectedTabIndex = selectedTabIndex,
+            indicator = {},
+        ) {
+            AnimatedTab(
+                title = "작성",
+                selected = selectedTabIndex == 0,
+                onClick = {
+                    selectedTabIndex = 0
+                },
+                modifier = Modifier.padding(
+                    horizontal = DialogTheme.spacing.extraSmall,
+                    vertical = DialogTheme.spacing.small,
+                )
+            )
+
+            AnimatedTab(
+                title = "미리보기",
+                selected = selectedTabIndex == 1,
+                onClick = {
+                    selectedTabIndex = 1
+                },
+                modifier = Modifier.padding(
+                    horizontal = DialogTheme.spacing.extraSmall,
+                    vertical = DialogTheme.spacing.small,
+                )
+            )
+        }
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(
+                    color = MaterialTheme.colorScheme.surfaceVariant,
+                    shape = DialogTheme.shapes.small
+                )
+                .padding(DialogTheme.spacing.large)
+        ) {
+            when (selectedTabIndex == 1) {
+                true -> {
+                    Markdown(
+                        content = text,
+                        colors = markdownColor(),
+                        typography = markdownTypography()
+                    )
+                }
+
+                false -> {
+                    Text(
+                        text = text,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MarkdownPreviewViewPreview() {
+    DialogTheme {
+        MarkdownPreviewView(
+            text = "#### Hello World\n\nThis is a **bold** statement.",
+            modifier = Modifier.size(500.dp, 300.dp)
+        )
+    }
+}
