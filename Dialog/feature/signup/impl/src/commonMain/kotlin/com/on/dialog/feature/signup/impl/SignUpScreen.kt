203f9258495d220392888f3fcd2c5796b5f5f0d8
ijh1298 <imjh0417@gmail.com>
2026-02-06 23:47:30 +0900

    feat: 회원가입 화면 UI 구현

--- Dialog/feature/signup/impl/src/commonMain/kotlin/com/on/dialog/feature/signup/impl/SignUpScreen.kt
+++ Dialog/feature/signup/impl/src/commonMain/kotlin/com/on/dialog/feature/signup/impl/SignUpScreen.kt
@@ -1,14 +1,24 @@
 package com.on.dialog.feature.signup.impl
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.on.dialog.designsystem.component.DialogButton
 import com.on.dialog.designsystem.component.DialogDropdownMenu
+import com.on.dialog.designsystem.component.DialogTopAppBar
 import com.on.dialog.designsystem.theme.DialogTheme
 import com.on.dialog.feature.signup.impl.mapper.toFullNameRes
 import com.on.dialog.model.common.Track
@@ -19,21 +29,46 @@ import kotlinx.collections.immutable.toImmutableList
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
-fun SignUpScreen(modifier: Modifier = Modifier) {
+fun SignUpScreen(
+    modifier: Modifier = Modifier
+) {
     val tracks: ImmutableList<String> =
-        Track.entries
-            .filter { it != Track.COMMON }
-            .map { stringResource(it.toFullNameRes()) }
+        Track.entries.filter { it != Track.COMMON }.map { stringResource(it.toFullNameRes()) }
             .toImmutableList()
 
-    var selectedIndex by rememberSaveable { mutableIntStateOf(0) }
+    var selectedIndex by rememberSaveable { mutableStateOf<Int?>(null) }
+    var notificationEnabled by rememberSaveable { mutableStateOf(false) }
 
-    DialogDropdownMenu(
-        options = tracks,
-        onSelectedIndexChange = { selectedIndex = it },
-        label = stringResource(Res.string.track),
-        selectedIndex = selectedIndex,
-    )
+    Column(modifier = modifier.fillMaxSize()) {
+        DialogTopAppBar(title = stringResource(Res.string.signup))
+
+        Column(
+            modifier = Modifier.padding(DialogTheme.spacing.large),
+            verticalArrangement = Arrangement.spacedBy(DialogTheme.spacing.medium),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            DialogDropdownMenu(
+                options = tracks,
+                onSelectedIndexChange = { selectedIndex = it },
+                label = stringResource(Res.string.track),
+                placeholder = "트랙을 선택해주세요",
+                selectedIndex = selectedIndex,
+            )
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Checkbox(
+                    checked = notificationEnabled, onCheckedChange = { notificationEnabled = it })
+                Text(text = "푸시 알림 수신 동의", style = DialogTheme.typography.bodyMedium)
+            }
+
+            DialogButton(
+                text = stringResource(Res.string.signup),
+                onClick = {}
+            )
+        }
+    }
 }
 
 @Preview
