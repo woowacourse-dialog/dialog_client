9595f63e9ca2302f91c49f3679575fc7346fbbfe
jerry8282 <ka06196@naver.com>
2026-02-09 21:03:23 +0900

    refactor: MarkdownEditor 초기 진입 시 커서 위치를 텍스트 끝으로 설정

--- Dialog/core/ui/src/commonMain/kotlin/com/on/dialog/ui/component/markdown/MarkdownEditor.kt
+++ Dialog/core/ui/src/commonMain/kotlin/com/on/dialog/ui/component/markdown/MarkdownEditor.kt
@@ -25,6 +25,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,7 +57,12 @@ fun MarkdownEditor(
         rememberNavigationEventState(NavigationEventInfo.None)
     val focusRequester: FocusRequester = remember { FocusRequester() }
     var content by rememberSaveable(stateSaver = TextFieldValue.Saver) {
-        mutableStateOf(TextFieldValue(initialContent))
+        mutableStateOf(
+            TextFieldValue(
+                text = initialContent,
+                selection = TextRange(initialContent.length)
+            )
+        )
     }
     var showExitDialog by rememberSaveable { mutableStateOf(false) }
 
