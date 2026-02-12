c906d60e0b47e32e808e1855acb0e2fd8c34fcb4
moondev03 <moondev03@gmail.com>
2026-02-13 00:39:35 +0900

    refactor: DialogButton의 Secondary 스타일 색상 및 프리뷰 방식 변경
    - core/designsystem:
        - `DialogButton`의 `Secondary` 스타일 색상을 `secondary`/`onSecondary`에서 `secondaryContainer`/`onSecondaryContainer`로 변경했습니다.
        - `@Preview` 어노테이션을 사용하는 대신 공통으로 사용되는 `@ThemePreview` 커스텀 어노테이션을 적용하여 라이트/다크 테마 미리보기를 통합했습니다.

--- Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/preview/ThemePreview.kt
+++ Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/preview/ThemePreview.kt
@@ -1,17 +1,16 @@
 package com.on.dialog.designsystem.preview
 
-import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_NO
-import androidx.compose.ui.tooling.preview.AndroidUiModes.UI_MODE_NIGHT_YES
+import androidx.compose.ui.tooling.preview.AndroidUiModes
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview(
     name = "LightTheme",
     showBackground = true,
-    uiMode = UI_MODE_NIGHT_NO,
+    uiMode = AndroidUiModes.UI_MODE_TYPE_NORMAL or AndroidUiModes.UI_MODE_NIGHT_NO,
 )
 @Preview(
     name = "DarkTheme",
     showBackground = true,
-    uiMode = UI_MODE_NIGHT_YES,
+    uiMode = AndroidUiModes.UI_MODE_TYPE_NORMAL or AndroidUiModes.UI_MODE_NIGHT_YES,
 )
 annotation class ThemePreview
