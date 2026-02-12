0feafa4c5d053e960be8de094670469dc2faf2b7
moondev03 <moondev03@gmail.com>
2026-02-13 01:08:01 +0900

    chore: 사용하지 않는 아이콘 import 제거
    - core/designsystem: `Icons.kt` 파일에서 더 이상 사용되지 않는 `Drafts`, `HourglassEmpty`, `RemoveCircle` 아이콘의 import 구문을 삭제합니다.

--- Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/icon/Icons.kt
+++ Dialog/core/designsystem/src/commonMain/kotlin/com/on/dialog/designsystem/icon/Icons.kt
@@ -13,7 +13,6 @@ import androidx.compose.material.icons.rounded.BookmarkBorder
 import androidx.compose.material.icons.rounded.Check
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.Delete
-import androidx.compose.material.icons.rounded.Drafts
 import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material.icons.rounded.Favorite
 import androidx.compose.material.icons.rounded.FavoriteBorder
@@ -22,14 +21,12 @@ import androidx.compose.material.icons.rounded.FilterAltOff
 import androidx.compose.material.icons.rounded.Forum
 import androidx.compose.material.icons.rounded.Group
 import androidx.compose.material.icons.rounded.Home
-import androidx.compose.material.icons.rounded.HourglassEmpty
 import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material.icons.rounded.Menu
 import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material.icons.rounded.Notifications
 import androidx.compose.material.icons.rounded.NotificationsOff
 import androidx.compose.material.icons.rounded.Person
-import androidx.compose.material.icons.rounded.RemoveCircle
 import androidx.compose.material.icons.rounded.SearchOff
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material.icons.rounded.Share
