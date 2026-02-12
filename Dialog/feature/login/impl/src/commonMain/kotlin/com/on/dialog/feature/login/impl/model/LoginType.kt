2b0f9d284fcfb9d7ccc9600c5c736cb6c3c79b7b
ijh1298 <imjh0417@gmail.com>
2026-02-12 14:10:23 +0900

    refactor: LoginType 내 URL 구성 방식 개선
    - `LOGIN_BASE_URL` 상수를 정의하여 중복되는 엔드포인트 경로 관리
    - `LoginType.GITHUB`의 `loginUrl` 정의 시 상수를 활용하도록 수정

--- Dialog/feature/login/impl/src/commonMain/kotlin/com/on/dialog/feature/login/impl/model/LoginType.kt
+++ Dialog/feature/login/impl/src/commonMain/kotlin/com/on/dialog/feature/login/impl/model/LoginType.kt
@@ -1,8 +1,10 @@
 package com.on.dialog.feature.login.impl.model
 
+private const val LOGIN_BASE_URL = "api/oauth2/authorization/"
+
 enum class LoginType(
     val loginUrl: String,
 ) {
-    GITHUB(loginUrl = "api/oauth2/authorization/github"),
+    GITHUB(loginUrl = LOGIN_BASE_URL + "github"),
     GOOGLE(loginUrl = ""),
 }
