bd74d32879181c30fe7819c7e8958dc096d83f6d
ijh1298 <imjh0417@gmail.com>
2026-02-15 22:35:45 +0900

    feat: DiscussionDetail 관련 UI 모델 구현
    - `DiscussionDetailUiModel`: 온라인/오프라인 상세 정보를 처리하는 Sealed Interface 및 하위 데이터 클래스 구현
    - `DetailContentUiModel`, `AuthorUiModel`: 토론 본문 및 작성자 정보를 담는 UI 모델 추가
    - `TrackUiModel`, `DiscussionStatusUiModel`: 트랙 정보 및 토론 상태 처리를 위한 Enum 클래스 및 도메인 모델 매퍼 구현
    - 도메인 모델(DiscussionDetail, Author, Track 등)을 UI 모델로 변환하는 확장 함수(`toUiModel`) 추가

---
+++ Dialog/feature/discussiondetail/impl/src/commonMain/kotlin/com/on/dialog/discussiondetail/impl/model/DetailContentUiModel.kt
@@ -0,0 +1,44 @@
+package com.on.dialog.discussiondetail.impl.model
+
+import com.on.dialog.discussiondetail.impl.model.DetailContentUiModel.AuthorUiModel.Companion.toUiModel
+import com.on.dialog.discussiondetail.impl.model.TrackUiModel.Companion.toUiModel
+import com.on.dialog.model.discussion.content.Author
+import com.on.dialog.model.discussion.content.DetailContent
+
+data class DetailContentUiModel(
+    val id: Long,
+    val title: String,
+    val author: AuthorUiModel,
+    val category: TrackUiModel,
+    val content: String,
+    val createdAt: String,
+    val likeCount: Int,
+    val modifiedAt: String,
+) {
+    data class AuthorUiModel(
+        val id: Long,
+        val nickname: String,
+        val imageUrl: String,
+    ) {
+        companion object {
+            fun Author.toUiModel(): AuthorUiModel = AuthorUiModel(
+                id = id,
+                nickname = nickname,
+                imageUrl = profileImage?.customImageUri ?: profileImage?.basicImageUri.orEmpty(),
+            )
+        }
+    }
+
+    companion object {
+        fun DetailContent.toUiModel(): DetailContentUiModel = DetailContentUiModel(
+            id = id,
+            title = title,
+            author = author.toUiModel(),
+            category = category.toUiModel(),
+            content = content,
+            createdAt = createdAt.toString(),
+            likeCount = likeCount,
+            modifiedAt = modifiedAt.toString(),
+        )
+    }
+}
