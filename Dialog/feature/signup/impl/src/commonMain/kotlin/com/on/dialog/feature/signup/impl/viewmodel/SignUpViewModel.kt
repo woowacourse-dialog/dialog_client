5371781af9166d764d22553f900efa44a363e4f4
ijh1298 <imjh0417@gmail.com>
2026-02-26 22:09:45 +0900

    feat: 로컬컬에 id 저장 실패 시 홈화면 이동 로직 추가

--- Dialog/feature/signup/impl/src/commonMain/kotlin/com/on/dialog/feature/signup/impl/viewmodel/SignUpViewModel.kt
+++ Dialog/feature/signup/impl/src/commonMain/kotlin/com/on/dialog/feature/signup/impl/viewmodel/SignUpViewModel.kt
@@ -68,6 +68,7 @@ class SignUpViewModel(
                         state = SnackbarState.NEGATIVE,
                     ),
                 )
+                emitEffect(SignUpEffect.NavigateHome)
             }
     }
 }
