c1c055560c35f74ab48269dd8198b82011cb6f85
moondev03 <moondev03@gmail.com>
2026-03-03 09:28:39 +0900

    refactor: 사용하지 않는 import 제거

--- Dialog/core/ui/src/commonMain/kotlin/com/on/dialog/ui/component/ProfileImage.kt
+++ Dialog/core/ui/src/commonMain/kotlin/com/on/dialog/ui/component/ProfileImage.kt
@@ -1,6 +1,5 @@
 package com.on.dialog.ui.component
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
